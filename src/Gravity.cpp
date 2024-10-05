a7e6e354c9811abb44e5f9fa3f8eb82626b3ef81
MathisMnr <96480780+MathisMnr@users.noreply.github.com>
2024-10-05 12:28:44 -0400

    Update Gravity.cpp

--- src/Gravity.cpp
+++ src/Gravity.cpp
@@ -10,18 +10,13 @@
 /**
  * @brief Default constructor
  */
-Gravity::Gravity()
-{
-	// TODO
-}
+Gravity::Gravity() : gravity(0.0, -9.81, 0.0, 0.0) {}
 
 /**
  * @brief Evaluated constructor
  */
-Gravity::Gravity(const Vector3d& g)
-{
-	// TODO
-}
+Gravity::Gravity(const Vector3d& g) : gravity(g) {}
+
 
 /**
  * @brief Update the force for a particle according to the duration
@@ -32,5 +27,15 @@ Gravity::Gravity(const Vector3d& g)
 */
 void Gravity::updateForce(Particle* particle, float duration)
 {
-	
+    // We check if the particle has a valid mass (finite)
+    if (particle->getInvertMass() <= 0.0f) return;
+
+    // Get the mass from the inverse mass
+    double mass = 1.0 / particle->getInvertMass();
+
+    // Calculate the gravitational force
+    Vector3d force = gravity * mass;
+
+    // Apply the force to the particle
+    particle->addForce(force);
 }
