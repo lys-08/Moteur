a90f97b9ae6446c4758b39c9c106b82cb352637b
Roméo Louati <romeolouati@gmail.com>
2024-10-19 23:03:14 -0400

    Force: ajout d'une surface pour la friction

--- src/particleStaticFriction.cpp
+++ src/particleStaticFriction.cpp
@@ -3,11 +3,13 @@
 particleStaticFriction::particleStaticFriction()
 {
 	mu_s = 0.5f;
+    surfacePlane = Plane(Vector3d(),Vector3d());
 }
 
-particleStaticFriction::particleStaticFriction(float coefficient)
+particleStaticFriction::particleStaticFriction(float coefficient, const Plane& plane)
 {
 	mu_s = coefficient;
+    surfacePlane = plane;
 }
 
 void particleStaticFriction::updateForce(Particle* particle, float duration)
@@ -17,19 +19,26 @@ void particleStaticFriction::updateForce(Particle* particle, float duration)
 
     // Get the accumulated force applied to the particle
     Vector3d appliedForce = particle->getForceAccum(); 
+    
+    Vector3d surfaceNormal = surfacePlane.getNormal();
+    surfaceNormal.normalise();
 
     // Calculate the normal force (reaction force to gravity)
     double mass = 1.0 / particle->getInvertMass();
-    Vector3d normalForce = Vector3d(0.0f, mass * 9.81f, 0.0f); // N = m * g // ONLY IN Y ?? // ONLY PLANE SURFACES ? OR INCLINED ?
+    Vector3d gravityForce = Vector3d(0.0f, mass * 9.81f, 0.0f); // N = m * g // ONLY IN Y ?? // ONLY PLANE SURFACES ? OR INCLINED ?
+
+    Vector3d normalForce = surfaceNormal * (gravityForce.dotProduct(surfaceNormal));
 
     // Calculate the maximum static friction force
     Vector3d maxStaticFriction = normalForce * mu_s;
 
+    Vector3d tangentialForce = appliedForce - surfaceNormal * appliedForce.dot(surfaceNormal);
+
     // If the applied force is less than or equal to the static friction force
-    if (appliedForce.norm() <= maxStaticFriction.norm())
+    if (tangentialForce.norm() <= maxStaticFriction.norm())
     {
         // Apply an opposite force to cancel out the movement (neutralize the force)
-        appliedForce *= -1;
+        tangentialForce *= -1;
         particle->addForce(appliedForce);
     } 
 
--- src/particleStaticFriction.h
+++ src/particleStaticFriction.h
@@ -7,19 +7,21 @@
 
 #include "particle.h"
 #include "particleForceGenerator.h"
+#include "Plane.h"
 
 class particleStaticFriction :
     public ParticleForceGenerator
 {
 private:
     float mu_s;  // Static friction coefficient
+    Plane surfacePlane;
 
 public:
     // Default constructor with a standard friction coefficient
     particleStaticFriction();
 
     // Constructor with a custom friction coefficient
-    particleStaticFriction(float coefficient);
+    particleStaticFriction(float coefficient, const Plane& plane);
 
     // Method to apply static friction force
     virtual void updateForce(Particle* particle, float duration) override;
