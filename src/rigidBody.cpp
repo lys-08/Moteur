16b0219efcff7dda48b353b6bca81a7a9e06c443
Lydie Santos <lydie.sts@gmail.com>
2024-11-06 19:05:05 -0500

    Suite du rigidBody

--- src/rigidBody.cpp
+++ src/rigidBody.cpp
@@ -12,9 +12,25 @@
 /**
  * @brief Default and evaluated constructor
  */
-RigidBody::RigidBody(Particle massCenter, Quaternion orientation)
+RigidBody::RigidBody(Particle massCenter, Quaternion rotation)
 {
-    // TODO
+    massCenter_ = massCenter;
+    rotation_ = rotation;
+    linearVelocity_ = Vector3d();
+    angularVelocity_ = Vector3d();
+}
+
+/**
+ * @brief Evaluated constructor
+ */
+RigidBody::RigidBody(float mass, Vector3d position = Vector3d(0, 0, 0, 1), Quaternion rotation = Quaternion())
+{
+    Vector3d speed = Vector3d();
+    massCenter_ = Particle(position, speed, 0, mass); // TODO typeDraw
+
+    rotation_ = rotation;
+    linearVelocity_ = Vector3d();
+    angularVelocity_ = Vector3d();
 }
 
 
@@ -33,9 +49,29 @@ Quaternion RigidBody::getRotation()
     return rotation_;
 }
 
+Vector3d RigidBody::getLinearVelocity()
+{
+    return linearVelocity_;
+}
+
+Vector3d RigidBody::getAngularVelocity()
+{
+    return angularVelocity_;
+}
+
 void RigidBody::setRotation(Quaternion rotation)
 {
     rotation_ = rotation;
+}
+
+void RigidBody::setLinearVelocity(Vector3d linearVelocity)
+{
+    linearVelocity_ = linearVelocity;
+}
+
+void RigidBody::setAngularVelocity(Vector3d angularVelocity)
+{
+    angularVelocity_ = angularVelocity;
 }
 
 
@@ -43,3 +79,36 @@ void RigidBody::setRotation(Quaternion rotation)
 // ============================================================================
 // Other methods ==============================================================
 // ============================================================================
+
+/**
+ * @brief add a force (apply to the center of mass) to the accumulator
+ *
+ * @param force the force to add to accumForce_
+ * @return nothing
+*/
+void RigidBody::addForce(const Vector3d& force)
+{
+    accumForce_ += force;
+}
+
+/**
+ * @brief add a force apply to a specific point to the accumulator
+ *
+ * @param force the force to add to accumForce_
+ * @param point the application point of the force
+ * @return nothing
+*/
+void RigidBody::addForceAtPoint(const Vector3d& force, const Vector3d& point)
+{
+    // TODO
+}
+
+/**
+ * @brief Clear accumForce
+ *
+ * @return nothing
+*/
+void RigidBody::clearAccumForce()
+{
+    accumForce_ = Vector3d(0, 0, 0, 0);
+}
--- src/rigidBody.h
+++ src/rigidBody.h
@@ -13,18 +13,30 @@
 class RigidBody
 {
 private:
-    Particle massCenter_;
+    Particle massCenter_; // countain the position
     Quaternion rotation_;
+    Vector3d linearVelocity_;
+    Vector3d angularVelocity_;
+
+    Vector3d accumForce_;
 
 
 public:
     // Constructors
-    RigidBody(Particle massCenter = Particle(), Quaternion orientation = Quaternion()); // TODO
+    RigidBody(Particle massCenter = Particle(), Quaternion rotation = Quaternion());
+    RigidBody(float mass, Vector3d position = Vector3d(0, 0, 0, 1), Quaternion rotation = Quaternion());
 
     // Getters and Setters
     Particle getMassCenter();
     Quaternion getRotation();
+    Vector3d getLinearVelocity();
+    Vector3d getAngularVelocity();
     void setRotation(Quaternion rotation);
+    void setLinearVelocity(Vector3d linearVelocity);
+    void setAngularVelocity(Vector3d angularVelocity);
 
     // Other methods
+    void addForce(const Vector3d& force);
+    void addForceAtPoint(const Vector3d& force, const Vector3d& point); // TODO
+    void clearAccumForce();
 };
